852abe5221722d7293b5b4dde55d2e01f5c9f27d
Tao Beloney <taorcb@gmail.com>
2026-01-11 00:29:21 -0600

    Use authenticated GitHub API requests (#7)
    Since none of our requests to the GitHub API are authenticated, the
    script would occasionally hit GitHub's rate limit for unauthenticated
    requests of 60/hr and fail with an unhelpful message.
    
    Now, if we have a GITHUB_TOKEN, we'll use it for authentication, which
    should eliminate the rate limiting problem.

--- Scripts/Modules/github-utils.psm1
+++ Scripts/Modules/github-utils.psm1
@@ -5,12 +5,14 @@ function Get-GitHubRelease() {
         [Parameter(Mandatory = $true)] [string] $FriendlyName
     )
 
+    $headers = Get-GitHubApiRequestHeaders
+
     if ($Version -eq 'latest') {
         Write-Host "Fetching latest $FriendlyName release info from GitHub..."
-        $release = Invoke-RestMethod "https://api.github.com/repos/$RepoSlug/releases/latest"
+        $release = Invoke-RestMethod "https://api.github.com/repos/$RepoSlug/releases/latest" -Headers $headers
     } else {
         Write-Host "Fetching $FriendlyName $Version release info from GitHub..."
-        $release = Invoke-RestMethod "https://api.github.com/repos/$RepoSlug/releases/tags/$Version"
+        $release = Invoke-RestMethod "https://api.github.com/repos/$RepoSlug/releases/tags/$Version" -Headers $headers
     }
 
     if (-not $release) {
@@ -33,4 +35,20 @@ function Get-ReleaseAsset() {
     }
 
     return $asset
+}
+
+function Get-GitHubApiRequestHeaders() {
+    # Build headers for authenticated requests
+    $headers = @{
+        'Accept'              = 'application/vnd.github+json'
+        'X-GitHub-Api-Version' = '2022-11-28'
+    }
+
+    if ($env:GITHUB_TOKEN) {
+        $headers['Authorization'] = "Bearer $env:GITHUB_TOKEN"
+    } else {
+        Write-Warning "GITHUB_TOKEN not set - using unauthenticated requests (rate limited to 60/hour)"
+    }
+
+    return $headers
 }
